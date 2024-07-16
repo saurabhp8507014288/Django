edb8c77d7ee1c677fc20740c48c285f400b12764
Saurabh Prakash <saurabhp85070@gmail.com>
2024-07-16 10:10:30 +0530

    let's try to deploy again

--- Task Management App/task_management/task_management/settings.py
+++ Task Management App/task_management/task_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y#=an4edlx86*$im18kqghq^q*pa_5++(n^v111zhahhatfzc2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
